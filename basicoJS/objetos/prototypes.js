ac8061c6e7c0cbd8e56f19ae106aee73ca01644b
Cristovao <cristovaofabio3@gmail.com>
2022-03-08 09:00:07 -0300

    finalizando prototype

--- basicoJS/objetos/prototypes.js
+++ basicoJS/objetos/prototypes.js
@@ -1,12 +1,37 @@
-function Person(name,age){
-    this.name=name,
-    this.age=age,
-    this.fullInfo = ()=> this.name + `, ${this.age}`;
+function Person(name, age) {
+    this.name = name,
+        this.age = age,
+        this.fullInfo = () => this.name + `, ${this.age}`;
 }
 
-Person.prototype.message="Hello!"
+Person.prototype.message = "Hello!"
 
-const person1 = new Person('Bob',20);
+const person1 = new Person('Bob', 20);
 
 console.log(person1.fullInfo());
 console.log(person1.message);
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+Product.prototype.discount = function (percentage) {
+    this.price = this.price * (100 - percentage) / 100;
+};
+
+function Car(name,price,doors){
+    Product.call(this,name,price);
+    this.doors=doors;
+};
+
+Car.prototype = Object.create(Product.prototype);
+Car.prototype.constructor = Car;
+
+const product1 = new Product('Shirt', 10);
+product1.discount(50);
+console.log(product1);
+
+const car1 = new Car('Lamborghini',1000000,2);
+car1.discount(10);
+console.log(car1);
