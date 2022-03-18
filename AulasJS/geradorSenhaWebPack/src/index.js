e6f789c1d4c122c0ea0ce1aaf535d7f94b14d9cd
Cristovao <cristovaofabio3@gmail.com>
2022-03-18 14:43:22 -0300

    delete file gerador de senha

--- AulasJS/geradorSenhaWebPack/src/index.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-import formGenaratonPassword from './modules/formGeneratorPassword';
-import './assets/css/style.css';
-
-formGenaratonPassword();
