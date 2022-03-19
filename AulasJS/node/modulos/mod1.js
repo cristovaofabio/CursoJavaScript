d19ec4e25960bc8625db211d13bd23c101892ff3
Cristovao <cristovaofabio3@gmail.com>
2022-03-19 09:59:22 -0300

    modules

---
+++ AulasJS/node/modulos/mod1.js
@@ -0,0 +1,9 @@
+const firstName = 'Mike';
+const lastName = 'Lory';
+
+const welcome = () => `Hi, my name is ${firstName} ${lastName}, welcome to my page!`;
+
+exports.lastName = lastName;
+exports.firstName = firstName;
+exports.welcome = welcome;
+this.someMessage = 'something to test';
