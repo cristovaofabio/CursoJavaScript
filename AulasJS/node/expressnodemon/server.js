6a45c7a2948957158152ba8a43d6f8464172a84a
Cristovao <cristovaofabio3@gmail.com>
2022-03-28 09:16:34 -0300

    connect to MongoDB

--- AulasJS/node/expressnodemon/server.js
+++ AulasJS/node/expressnodemon/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        //console.log('Connection to database established!');
+        app.emit('all right!');
+    })
+    .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const myMiddleware = require('./src/middlewares/middleware');
@@ -12,10 +23,11 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(myMiddleware);
-
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('The server is executing!!!')
-    console.log('Check this link: http://localhost:3000');
+app.on('all right!', () => {
+    app.listen(3000, () => {
+        console.log('The server is executing...')
+        console.log('Check this link: http://localhost:3000');
+    });
 });
--- AulasJS/node/expressnodemon/src/controllers/homeController.js
+++ AulasJS/node/expressnodemon/src/controllers/homeController.js
@@ -1,3 +1,15 @@
+/*
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+    title: 'Some title',
+    description: 'Some description'
+})
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+*/
+
 exports.homePage = (req, res) => {
     res.render('index');
     return;
