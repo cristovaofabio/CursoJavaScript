52a4707f4165ed9d2296531aca046397cb72fab3
Cristovao <cristovaofabio3@gmail.com>
2022-04-13 19:59:32 -0300

    api rest - students CRUD

--- AulasJS/apirest/src/controllers/StudentController.js
+++ AulasJS/apirest/src/controllers/StudentController.js
@@ -5,6 +5,121 @@ class StudentController {
     const students = await Student.findAll();
     res.json(students);
   }
+
+  async store(req, res) {
+    try {
+      const student = await Student.create(req.body);
+      return res.json(student);
+    } catch (e) {
+      return res.status(400).json(
+        {
+          errors: e.errors.map((err) => err.message),
+        },
+      );
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json(
+          {
+            errors: ['missing id'],
+          },
+        );
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json(
+          {
+            errors: ['studant not found'],
+          },
+        );
+      }
+
+      return res.json(student);
+    } catch (e) {
+      return res.status(400).json(
+        {
+          errors: e.errors.map((err) => err.message),
+        },
+      );
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json(
+          {
+            errors: ['missing id'],
+          },
+        );
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json(
+          {
+            errors: ['studant not found'],
+          },
+        );
+      }
+
+      const newStudent = await student.update(req.body);
+
+      return res.json(newStudent);
+    } catch (e) {
+      return res.status(400).json(
+        {
+          errors: e.errors.map((err) => err.message),
+        },
+      );
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json(
+          {
+            errors: ['missing id'],
+          },
+        );
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json(
+          {
+            errors: ['student not found'],
+          },
+        );
+      }
+
+      await student.destroy();
+
+      return res.json({
+        message: 'student deleted ',
+      });
+    } catch (e) {
+      return res.status(400).json(
+        {
+          errors: e.errors.map((err) => err.message),
+        },
+      );
+    }
+  }
 }
 
 export default new StudentController();
--- AulasJS/apirest/src/models/Student.js
+++ AulasJS/apirest/src/models/Student.js
@@ -4,12 +4,65 @@ export default class Student extends Model {
   static init(sequelize) {
     super.init(
       {
-        firstname: Sequelize.STRING,
-        lastname: Sequelize.STRING,
-        email: Sequelize.STRING,
-        age: Sequelize.INTEGER,
-        weight: Sequelize.FLOAT,
-        height: Sequelize.FLOAT,
+        firstname: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 200],
+              msg: 'the first name length should be between 3 and 200 characters',
+            },
+          },
+        },
+        lastname: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 200],
+              msg: 'the last first name length should be between 3 and 200 characters',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'email already in use',
+          },
+          validate: {
+            isEmail: {
+              msg: 'invalid email',
+            },
+          },
+        },
+        age: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+          validate: {
+            isInt: {
+              msg: 'the age needs be a integer number',
+            },
+          },
+        },
+        weight: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'the weight needs be a number',
+            },
+          },
+        },
+        height: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'the height needs be a number',
+            },
+          },
+        },
       },
       {
         sequelize,
